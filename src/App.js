df472afa9e9a44044f39a8191c6b692933eb745d
Mohammed Huzaifa <huzaifafcrit@gmail.com>
2024-01-07 20:26:18 -0500

    Added list of pokemons, some html css + search logic using React useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
+import pokemons from './pokemons.json';
+import React from 'react';
+
+const PokemonRow = ({ pokemon }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(', ')}</td>
+  </tr>);
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string
+    }),
+    type: PropTypes.arrayOf(PropTypes.string)
+  })
+};
 
 function App() {
+
+  const [getSearch, setSearch] = React.useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        margin: "auto",
+        width: "100%",
+      }}
+    >
+      <h1 className="title">Pokemon Search</h1>
+      <input
+        value={getSearch}
+        onChange={(event)=>setSearch(event.target.value)}
+        placeholder='Search for a pokemon...'
+      />
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            pokemons
+              .slice(0, 20)
+              .filter((pokemon) => pokemon.name.english.toLowerCase().includes(getSearch.toLowerCase()))
+              .map((pokemon) => (
+              <PokemonRow pokemon={pokemon} key={pokemon.id} />
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
